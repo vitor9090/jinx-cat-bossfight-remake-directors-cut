b5faef0436350472e4537673f2a7fc634a833d89
vitor9090 <vitorgabrielgomesro9090@gmail.com>
2022-05-16 12:15:51 -0300

    first commir

---
+++ main.py
@@ -0,0 +1,10 @@
+#/anaconda3/envs/py310/bin/python
+from ursina import *
+
+# I'm probably going to make a first person controller, not now tho (12:15) since it's almost school time.
+
+def main() -> None:
+    pass
+
+if __name__ == '__main__':
+    main()   
